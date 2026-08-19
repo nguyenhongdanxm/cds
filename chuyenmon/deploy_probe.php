b7e898b61dbff8b863c3170ffa3a03d39690652e
nguyenhongdanxm <nguyenhongdan@noitruxinman.edu.vn>
2026-08-19 19:00:14 +0700

    Add chuyenmon deployment source probe

---
+++ chuyenmon/deploy_probe.php
@@ -0,0 +1,7 @@
+<?php
+header('Content-Type: text/plain; charset=utf-8');
+echo "CDS CHUYENMON DEPLOY PROBE\n";
+echo "SOURCE: main/chuyenmon\n";
+echo "VERSION: 2026-08-19-1900-tkb-color-v3\n";
+echo "FILE: ".__FILE__."\n";
+echo "TIME: ".date('c')."\n";
