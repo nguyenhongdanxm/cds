f55246ec8f1032f7bb36bdc60b07b054ab886a3a
nguyenhongdanxm <nguyenhongdan@noitruxinman.edu.vn>
2026-08-23 17:54:03 +0700

    Open KTX exit registration first when available

--- noitru_exit_manager.php
+++ noitru_exit_manager.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/includes/auth.php';
 require_login();
 require_perm('nt.ravao');
 $query = $_GET;
-$target = BASE_URL . 'noitru_exit.php' . ($query ? '?' . http_build_query($query) : '');
+if (!isset($query['view'])) $query['view'] = 'register';
+$target = BASE_URL . 'noitru_exit.php?' . http_build_query($query);
 header('Location: ' . $target);
 exit;
