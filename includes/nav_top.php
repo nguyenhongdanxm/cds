fdcbbeeb3663782e12ca3a1fdc9fb889a1be6652
nguyenhongdanxm <nguyenhongdan@noitruxinman.edu.vn>
2026-08-28 11:20:14 +0700

    Load permission tree UI on users page

--- includes/nav_top.php
+++ includes/nav_top.php
@@ -91,3 +91,6 @@ body > nav.navbar-dark:not(.cds-nav){display:none!important}
   </div>
 </nav>
 <?php require_once __DIR__.'/module_switcher.php'; ?>
+<?php if (basename($_SERVER['SCRIPT_NAME'] ?? '') === 'users.php'): ?>
+<script src="<?= e(BASE_URL . 'assets/users_permission_tree.js?v=20260828-1') ?>" defer></script>
+<?php endif; ?>
