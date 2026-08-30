5f39bc036251b94ac30053679ef2254f2636b965
nguyenhongdanxm <nguyenhongdan@noitruxinman.edu.vn>
2026-08-30 07:46:06 +0700

    Phase 2: expose shared school identity constants

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Cổng dữ liệu số (CDS) - Hệ sinh thái số nhà trường
- * Deploy: cds.noitruxinman.edu.vn
+ * Cổng dữ liệu số (CDS) - Hệ sinh thái số nhà trường.
+ * Thông tin nhận diện trường được quản lý tập trung tại school_config.php.
  */
 define('BASE_PATH', dirname(__DIR__));
 define('DATA_PATH', BASE_PATH . '/data');
@@ -17,6 +17,14 @@ define('SCHOOL_NAME', school_name());
 define('SCHOOL_SHORT', school_short_name());
 define('SCHOOL_SO', school_department());
 define('SCHOOL_YEAR', school_year());
+define('SCHOOL_CODE', school_code());
+define('SCHOOL_ADDRESS', school_address());
+define('SCHOOL_PHONE', school_phone());
+define('SCHOOL_EMAIL', school_email());
+define('SCHOOL_LOGO', school_logo());
+define('CDS_TITLE', school_cds_title());
+define('CDS_SHORT_TITLE', school_cds_short_title());
+define('CDS_DESCRIPTION', school_description());
 
 define('URL_TIN_TUC', school_website());
 /* Chuyên môn (PCCM) – module thư mục trên cùng domain CDS */
