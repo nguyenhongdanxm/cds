7f29b8dccaa199aeaa074a53523767b6638a9961
nguyenhongdanxm <nguyenhongdan@noitruxinman.edu.vn>
2026-09-07 19:38:12 +0700

    Hide overview and reorder primary Chuyen mon menu

--- chuyenmon/includes/cds_responsive_layout.php
+++ chuyenmon/includes/cds_responsive_layout.php
@@ -25,10 +25,9 @@ $cmReportActive = in_array($current,['dugio','kiemtrahoso','danhgia'],true);
 
 $cmNavGroups = [
     ['label'=>'Tổng quan','items'=>[
-        ['permission'=>'cm.dashboard','pages'=>['index'],'href'=>'index.php','icon'=>'bi-house-door','label'=>'Tổng quan & công việc'],
         ['permission'=>'cm.dashboard','pages'=>['sodaubai'],'href'=>'sodaubai.php','icon'=>'bi-journal-check','label'=>'Sổ đầu bài'],
-        ['permission'=>'cm.tracuu','pages'=>['tracuu'],'href'=>'tracuu.php','icon'=>'bi-search','label'=>'Tra cứu phân công'],
         ['permission'=>'cm.tracuu','pages'=>['thoikhoabieu'],'href'=>'thoikhoabieu.php','icon'=>'bi-calendar3','label'=>'Thời khóa biểu'],
+        ['permission'=>'cm.tracuu','pages'=>['tracuu'],'href'=>'tracuu.php','icon'=>'bi-search','label'=>'Tra cứu phân công'],
     ]],
     ['label'=>'Phân công – Danh mục','items'=>[
         ['permission'=>'cm.pccm','pages'=>['tongquan'],'href'=>'tongquan.php','icon'=>'bi-grid','label'=>'Tổng quan PCCM'],
