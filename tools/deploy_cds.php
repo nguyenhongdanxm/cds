4fb570d17ae84e71ee505425a2df9d16c074eb9b
nguyenhongdanxm <nguyenhongdan@noitruxinman.edu.vn>
2026-09-09 14:50:24 +0700

    Deploy duty report ZIP endpoint

--- tools/deploy_cds.php
+++ tools/deploy_cds.php
@@ -74,7 +74,7 @@ $rootFiles = [
     'chuyenmon.php','csdl.php','csdl_preweeks.php','csdl_export.php','csdl_export_filtered_excel.php',
     'csdl_statistics_export_xlsx.php','csdl_student_cards.php','csdl_staff_cards.php','staff_card_teachers.php','staff_photo.php','staff_verify.php','danhgia.php','dashboard_settings.php',
     'database_admin.php','instance_settings.php','initial_setup.php','index.php','login.php','logout.php','manifest.php','manifest.webmanifest','sw.js',
-    'noitru.php','noitru_overview_api.php','noitru_duty_drive.php','noitru_exit.php','noitru_exit_manager.php',
+    'noitru.php','noitru_overview_api.php','noitru_duty_drive.php','noitru_duty_reports_zip.php','noitru_exit.php','noitru_exit_manager.php',
     'noitru_exit_drive_api.php','noitru_exit_check_api.php','noitru_exit_check.php','noitru_attendance.php',
     'noitru_list.php','noitru_assign.php','noitru_assign_enhanced.php','noitru_assign_sync.php',
     'noitru_room_template.php','noitru_room_roles.php','noitru_room_roles_data.php','noitru_room_quick_save.php',
